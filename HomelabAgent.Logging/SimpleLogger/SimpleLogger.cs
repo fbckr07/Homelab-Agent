ac79eb0b18a828af45ffe710f655607c70760aab
Felix Becker <felixbecker0312@gmail.com>
2026-07-14 02:24:26 +0200

    Add logging project and implement simple logging functionality

---
+++ HomelabAgent.Logging/SimpleLogger/SimpleLogger.cs
@@ -0,0 +1,39 @@
+namespace HomelabAgent.Logging.SimpleLogger;
+
+public static class SimpleLogger
+{
+    public static void LogInformation(string message)
+    {
+        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"[INFO] [{timestamp}]  {message}");
+        Console.ResetColor();
+    }
+    
+    public static void LogWarning(string message)
+    {
+        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"[WARN] [{timestamp}]  {message}");
+        Console.ResetColor();
+    }
+
+    public static void LogError(string message)
+    {
+        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[ERROR] [{timestamp}]  {message}");
+        Console.ResetColor();
+    }
+    
+    public static void LogException(Exception exception, bool ExitApplication = false)
+    {
+        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[EXCEPTION] [{timestamp}]  {exception.Message}");
+        Console.ResetColor();
+        
+        if (ExitApplication)
+            Environment.Exit(1);
+    }
+}
