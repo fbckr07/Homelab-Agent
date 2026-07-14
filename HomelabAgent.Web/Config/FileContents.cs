8d0de64cc867ef1ecacd602f228ab2789d59495e
Felix Becker <felixbecker0312@gmail.com>
2026-07-14 11:44:54 +0200

    Reorder ConfigFileContents declaration in FileContents class for clarity

--- HomelabAgent.Web/Config/FileContents.cs
+++ HomelabAgent.Web/Config/FileContents.cs
@@ -2,16 +2,16 @@ namespace HomelabAgent.Web.Config;
 
 public static class FileContents
 {
-    public static Dictionary<string, string> ConfigFileContents = new()
-    {
-        { "host.ini", HostConfigContent },
-        { "agent.ini", string.Empty}
-    };
-
-
     private static string HostConfigContent = """
                                               [Host]
                                               HostAdress=1.1.1.1,
                                               HostPort=33655
                                               """;
+    
+    
+    public static Dictionary<string, string> ConfigFileContents = new()
+    {
+        { "host.ini", HostConfigContent },
+        { "agent.ini", string.Empty}
+    };
 }
